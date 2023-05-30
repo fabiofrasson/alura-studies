9a9dd5e9ba4da92667b4e7248e5fb212b73184cd
Fabio Frasson <fabio.frass@gmail.com>
2023-05-30 16:22:32 -0300

    Controlando o formulário

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -3,15 +3,31 @@ import Button from "../Button";
 import style from "./Form.module.scss";
 
 class Form extends React.Component {
+    state = {
+        task: "",
+        time: "00:00:00",
+    };
+
+    addTask(event: React.FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+        console.log("state: ", this.state);
+    }
     render() {
         return (
-            <form className={style.newTask}>
+            <form className={style.newTask} onSubmit={this.addTask.bind(this)}>
                 <div className={style.inputContainer}>
                     <label htmlFor="task">Adicione um novo estudo</label>
                     <input
                         type="text"
                         name="task"
                         id="task"
+                        value={this.state.task}
+                        onChange={(event) =>
+                            this.setState({
+                                ...this.state,
+                                task: event.target.value,
+                            })
+                        }
                         placeholder="O que você quer estudar"
                         required
                     />
@@ -22,9 +38,17 @@ class Form extends React.Component {
                         type="time"
                         step="1"
                         name="time"
+                        value={this.state.time}
+                        onChange={(event) =>
+                            this.setState({
+                                ...this.state,
+                                time: event.target.value,
+                            })
+                        }
                         id="time"
                         min="00:00:00"
-                        max="01:30:00"
+                        max="23:59:59"
+                        //pattern="\d{2}:\d{2}"
                         required
                     />
                 </div>
