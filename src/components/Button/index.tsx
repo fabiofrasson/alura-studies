3a36c7ab654249be75577584e9a7423994cfe3d0
Fabio Frasson <fabio.frass@gmail.com>
2023-05-30 09:55:30 -0300

    Aprendendo a estilizar

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,7 +2,11 @@ import React from "react";
 
 class Button extends React.Component {
     render() {
-        return <button>Button</button>;
+        const isActive = true;
+        const styles = {
+            backgroundColor: isActive ? "green" : "red",
+        };
+        return <button style={styles}>Button</button>;
     }
 }
 
